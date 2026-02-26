8cfac85d96cf85fe403e728f9397df9bbf43c0f2
Justin Mondal <mondal.25@osu.edu>
2026-02-26 05:35:43 -0500

    Update LiveCameraView.swift

--- Sources/ExyteMediaPicker/Screens/Camera/LiveCameraView.swift
+++ Sources/ExyteMediaPicker/Screens/Camera/LiveCameraView.swift
@@ -1,6 +1,6 @@
 //
 //  LiveCameraView.swift
-//  
+//
 //
 //  Created by Alexandra Afonasova on 18.10.2022.
 //
@@ -23,7 +23,20 @@ public struct LiveCameraView: UIViewRepresentable {
         )
     }
 
-    public func updateUIView(_ uiView: LiveVideoCaptureView, context: Context) { }
+    public func updateUIView(_ uiView: LiveVideoCaptureView, context: Context) {
+        uiView.setOrientation(orientation)
+    }
+}
+
+// MARK: - AVCaptureVideoOrientation from UIDeviceOrientation
+private func captureVideoOrientation(from deviceOrientation: UIDeviceOrientation) -> AVCaptureVideoOrientation {
+    switch deviceOrientation {
+    case .portrait: return .portrait
+    case .portraitUpsideDown: return .portraitUpsideDown
+    case .landscapeLeft: return .landscapeRight
+    case .landscapeRight: return .landscapeLeft
+    default: return .portrait
+    }
 }
 
 public final class LiveVideoCaptureView: UIView {
@@ -41,7 +54,10 @@ public final class LiveVideoCaptureView: UIView {
 
     private var videoLayer: AVCaptureVideoPreviewLayer { layer as! AVCaptureVideoPreviewLayer }
 
+    private var currentOrientation: UIDeviceOrientation
+
     required init?(coder: NSCoder) {
+        self.currentOrientation = .portrait
         super.init(coder: coder)
     }
 
@@ -51,8 +67,27 @@ public final class LiveVideoCaptureView: UIView {
         videoGravity: AVLayerVideoGravity = .resizeAspect,
         orientation: UIDeviceOrientation
     ) {
+        self.currentOrientation = orientation
         super.init(frame: frame)
         self.session = session
         videoLayer.videoGravity = videoGravity
+        applyPreviewOrientation()
+    }
+
+    func setOrientation(_ orientation: UIDeviceOrientation) {
+        guard orientation != currentOrientation else { return }
+        currentOrientation = orientation
+        applyPreviewOrientation()
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        videoLayer.frame = bounds
+        applyPreviewOrientation()
+    }
+
+    private func applyPreviewOrientation() {
+        guard let connection = videoLayer.connection, connection.isVideoOrientationSupported else { return }
+        connection.videoOrientation = captureVideoOrientation(from: currentOrientation)
     }
 }
